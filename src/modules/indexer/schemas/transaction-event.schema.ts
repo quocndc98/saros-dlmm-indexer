1ddce2d987d5ff40a89dea01ebe3e97869362415
quocndc98 <quocnd@coin98.finance>
2025-08-18 13:13:56 +0700

    feat: filter out failed transaction

--- src/modules/indexer/schemas/transaction-event.schema.ts
+++ src/modules/indexer/schemas/transaction-event.schema.ts
@@ -13,22 +13,10 @@ export class TransactionEvent extends Document {
   blockTime: Date
 
   @Prop({ required: true })
-  isSuccessful: boolean
-
-  @Prop({ type: Object })
-  errorMessage?: any
-
-  @Prop({ required: true })
   processed: boolean
 
   @Prop({ default: false })
   queued: boolean
-
-  @Prop({ default: Date.now })
-  createdAt: Date
-
-  @Prop({ default: Date.now })
-  updatedAt: Date
 }
 
 export const TransactionEventSchema = SchemaFactory.createForClass(TransactionEvent)
